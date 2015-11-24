2928e49f3f1970c27e4fe04d99aa607b11f8a8b6
josh <joshsmith2@gmail.com>
2015-11-24 21:36:27 +0000

    Whole load script creation working. Needs translations and testing on Qlik.

--- load_from_script.py
+++ load_from_script.py
@@ -3,7 +3,6 @@ __author__ = 'josh'
 import csv
 import argparse
 
-
 def get_arguments():
     """
     Parse command line arguments
@@ -15,7 +14,16 @@ def get_arguments():
                                                  "CSV.")
     parser.add_argument("-c", "--csv-file", metavar="PATH", type="str",
                         dest='csv', help="The CSV file to read")
+    parser.add_argument("-o", "--out-file", metavar="PATH", type="str",
+                        dest="out_file", help="The load script")
+    parser.add_argument("-h", "--header-file", metavar="PATH", type="str",
+                        dest="header_file",
+                        help="Path to a file containing a standard load script"
+                             " header - usually a bunch of SET statements for "
+                             "timestamps etc")
+
     return parser.parse_args()
+
 
 def get_headers(from_file):
     """
@@ -28,10 +36,62 @@ def get_headers(from_file):
         reader = csv.DictReader(ff)
         return reader.fieldnames
 
+
+def print_header(header_file, out_file):
+    """
+    Print the Qlikview header to the out file
+
+    :param header_file: A .txt file containing the desired Qlikview header
+    (Usually a bunch of SET statements for DateTime formats etc)
+    :param out_file: File to write to
+    """
+    with open(out_file, 'w') as of:
+        with open(header_file, 'r') as hf:
+            for line in hf.readlines():
+                of.write(line)
+
+
+def print_table_writer(out_file, headers, csv_file):
+    """
+    Print a load script to write headers into out_file
+    """
+    # Name of the table in QV to store the data into
+    table_name = "tweets"
+
+    with open(out_file, 'a') as o:
+        o.write("\n")
+        o.write("{}:\n".format(table_name))
+        o.write("LOAD\n")
+        for header in headers[:-1]:
+            o.write("\t[{}],\n".format(header))
+        o.write("\t[{}]\n\n".format(headers[-1]))
+        o.write("FROM\n")
+        o.write("[{}]\n".format(csv_file))
+        o.write("(txt, codepage is 1252, embedded labels, delimiter is ',', "
+                "msq);")
+
+
 def main():
+    out_file_default = "load_out.txt"
+    header_file_default = "standard_contents/header.txt"
+
     args = get_arguments()
     csv_path = args.csv
+
+    try:
+        out_file = args.out_file
+    except:
+        out_file = out_file_default
+
+    # If header file exists, print it to the output file
+    try:
+        header_file = args.header_file
+    except:
+        header_file = header_file_default
+
+    print_header(header_file, out_file)
     headers = get_headers(csv_path)
+
 
 if __name__ == "__main__":
     main()
--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -2,13 +2,37 @@ __author__ = 'josh'
 
 import unittest
 import load_from_script
+import os
 
 class UnitTest(unittest.TestCase):
 
     def setUp(self):
         self.in_csv = "files/clutha_sample.csv"
+        self.header_file = "../standard_contents/header.txt"
+        self.out_file = "out.txt"
+        if os.path.exists(self.out_file):
+            os.remove(self.out_file)
+
+    def tearDown(self):
+        pass
 
     def test_headers(self):
         headers = load_from_script.get_headers(self.in_csv)
         self.assertTrue(len(headers) == 93)
         self.assertIn("twitter.tweet/truncated", headers)
+
+    def test_ls_header(self):
+        load_from_script.print_header(self.header_file, self.out_file)
+        self.assertTrue(os.path.exists(self.out_file))
+        with open(self.header_file, 'r') as hf:
+            expected = hf.readlines()
+        with open(self.out_file, 'r') as of:
+            observed = of.readlines()
+        self.assertEqual(expected, observed)
+
+    def test_table_statement(self):
+        headers = load_from_script.get_headers(self.in_csv)
+        load_from_script.print_table_writer(self.out_file,
+                                            headers,
+                                            self.in_csv)
+        self.assertTrue(os.path.exists(self.out_file))
